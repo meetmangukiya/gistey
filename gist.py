a7e83a6293ce5ab40305632bf56b4fa2b338c6a2
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-12-02 15:42:58 +0530

    gist.py: Add file processing function
    Read all the files that are passed in the arguments,
    and return a dictionary containing `key` as file name
    and `value` as file contents

--- gist.py
+++ gist.py
@@ -4,3 +4,18 @@ import sys
 from parser import parser
 
 args = parser.parse_args()
+
+def process_files():
+    files = [os.path.abspath(file) for file in args.files]
+    file_contents = {}
+    for file in files:
+        try:
+            f = open('file')
+            file_contents[os.path.split(file)[1]] = f.read()
+            f.close()
+        except FileNotFoundError:
+            print('File "{}"\n\tdoes not exist'.format(file))
+            should_create = input('Create the gist without this file [Y/n]: ') or 'Y'
+            if not should_create == 'Y':
+                sys.exit("gist: exiting ...")
+            return file_contents
