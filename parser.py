241f2fe16e85e8c502b79530a930be4c46f68627
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-12-02 15:40:24 +0530

    parser.py: To parse the arguments

---
+++ parser.py
@@ -0,0 +1,14 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Create github gists from cmdline",
+                                 prog="gist")
+
+parser.add_argument('-s', '--secret',
+                    default=False,
+                    help="To create a secret gist",
+                    action='store_true')
+
+parser.add_argument('-f','--files',
+                    nargs='+',
+                    help="Files to be uploaded in the gist",
+                    action='store')
