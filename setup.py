50d5b1c183f735f15c3455fdb81db45d91f9f358
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-12-03 15:47:43 +0530

    setup.py: Add setup.py and make gistey ready for distribution
    Closes https://github.com/meetmangukiya/gist-cli/issues/8

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('requirements.txt') as req:
+    dependencies = req.read().splitlines()
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+if __name__ == "__main__":
+    setup(
+        name='gist',
+        version="0.1.0",
+        description="Make GitHub gists from cmdline/terminal",
+        author="Meet Mangukiya",
+        author_email="meetmangukiya98@gmail.com",
+        maintainer="Meet Mangukiya",
+        maintainer_email=("meetmangukiya98@gmail.com", ),
+        url="http://meetmangukiya.github.io",
+        install_requires=dependencies,
+        license='MIT',
+        py_modules=['gist', 'ArgumentParser'],
+        long_description=long_description,
+        entry_points = {
+            'console_scripts': [
+                'gistey=gist:main'
+            ]
+        }
+    )
