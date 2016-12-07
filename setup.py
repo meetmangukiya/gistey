9dafa7a96c407cd53f4ceb01bf9d61d0dfec2055
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-12-07 18:50:03 +0530

    setup.py: Add README.rst to long description
    Fixes https://github.com/meetmangukiya/gistey/issues/23

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open('requirements.txt') as req:
     dependencies = req.read().splitlines()
 
-with open('README.md') as readme:
+with open('README.rst') as readme:
     long_description = readme.read()
 
 if __name__ == "__main__":
@@ -11,6 +11,7 @@ if __name__ == "__main__":
         name='gistey',
         version="0.1.0",
         description="Make GitHub gists from cmdline/terminal",
+        long_description=long_description,
         author="Meet Mangukiya",
         author_email="meetmangukiya98@gmail.com",
         maintainer="Meet Mangukiya",
